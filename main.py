843cf87f0aea64e1512ca96bad127734bea021e5
user <swyat.usatov@gmail.com>
2020-12-12 01:28:21 +0200

    -add menu

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+import pygame_menu
+
+pygame.init()
+surface = pygame.display.set_mode((1920, 1080))
+menu = pygame_menu.Menu(1080, 1920, 'Welcome',
+                        theme=pygame_menu.themes.THEME_DARK)
+
+def start_the_game():
+    pass
+
+menu.add_image("first.png")
+menu.add_button('Play', start_the_game)
+
+menu.add_button('Quit', pygame_menu.events.EXIT)
+menu.mainloop(surface)
