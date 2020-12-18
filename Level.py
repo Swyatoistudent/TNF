715405e9b928567a08477244a9de559f21837fe0
Sviatoslav Usatov <55549187+Swyatoistudent@users.noreply.github.com>
2020-12-18 22:19:12 +0200

    Update Level.py

--- Level.py
+++ Level.py
@@ -6,7 +6,7 @@ class Level:
 
     def __init__(self):
         self.__rooms = game_objects_data[id]['rooms']
-        self.__current_room = 0
+        self.__current_room = [0,0]
 
     def get_map(self):
         return [
